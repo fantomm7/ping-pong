62b407b2697d749e8cd82a9c593223c0174e380c
fantomm7 <129409853+fantomm7@users.noreply.github.com>
2023-04-13 18:19:23 +0300

    v2
    It's final version. Maybe...

--- ping-pong.py
+++ ping-pong.py
@@ -1,7 +1,8 @@
 from pygame import *
 from random import randint
-#from time import sleep
+from time import sleep
 #from time import time as timer 
+
 
 back = (150, 200, 176)
 wh = 500
@@ -12,11 +13,15 @@ clock = time.Clock()
 window.fill(back)
 FPS = 60
 font.init()
-font = font.Font(None, 25)
+font = font.Font(None, 72)
 lose1 = font.render("1 lose!", True, (150, 0, 0))
 lose2 = font.render("2 lose!", True, (150, 0, 0))
 game = True
 finish = False
+fatalLeft = 0
+fatalRight = 0
+scoreL = font.render("Score: " + str(fatalRight), True, (0, 160, 0))
+scoreR = font.render("Score: " + str(fatalLeft), True, (0, 160, 0))
 
 
 class GameSprite(sprite.Sprite):
@@ -44,11 +49,15 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y <= wh-200:
             self.rect.y += self.s
 
-r1 = Player("rect.png", 100, 200, 75, 0, 5)
-r2 = Player("rect.png", 100, 200, ww-175, 0, 5)
-ball = GameSprite("ball.png", 50, 50, 350, 250, 3)
-sx = 3
-sy = 3
+r1 = Player("rect.png", 50, 200, 10, 0, 10)
+r2 = Player("rect.png", 50, 200, ww-60, 0, 10)
+ball = GameSprite("ball.png", 50, 50, 250, 250, 0)
+ball2 = GameSprite("ball.png", 50, 50, 450, 250, 0)
+sx = 2
+sy = 2
+sx2 = 3
+sy2 = 3
+bits = 0 
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -59,20 +68,60 @@ while game:
         r2.update_r()
         r1.move()
         r2.move()
-        ball.move()
         ball.rect.x += sx
         ball.rect.y += sy
+        ball.move()
+        ball2.rect.x += sx2
+        ball2.rect.y += sy2
+        ball2.move()
+        window.blit(scoreL, (10, 10))
+        window.blit(scoreR, (ww-220, 10))
         if sprite.collide_rect(r1, ball) or sprite.collide_rect(r2, ball):
-            speed_x *= -1
-            sy *= 1
+            sx *= -randint(700, 1300)/1000
+            sy *= randint(500, 1500)/1000
+            bits += 1
+            if (bits > randint(0, 5)):
+                bits = 0
+                sx *= 1.33
+                sy *= 1.25
+        if sprite.collide_rect(r1, ball2) or sprite.collide_rect(r2, ball2):
+            sx2 *= -randint(700, 1300)/1000
+            sy2 *= randint(500, 1500)/1000
+            if (bits > randint(0, 5)):
+                bits = 0
+                sx2 *= 1.33
+                sy2 *= 1.25
         if ball.rect.y >= wh-50 or ball.rect.y <= 0:
-            sy *= -1
-        if ball.rect.x <= 0:
-            finish = True
-            window.blit(lose1, (200, 200))
-        if ball.rect.x >= ww:
-            finish = True
-            window.blit(lose2, (200, 200))
-
+            sy *= -randint(500, 1500)/1000
+        if ball2.rect.y >= wh-50 or ball2.rect.y <= 0:
+            sy2 *= -randint(500, 1500)/1000
+        if ball.rect.x <= -10 or ball2.rect.x <= -10:
+            fatalLeft += 1
+            scoreR = font.render("Score: " + str(fatalLeft), True, (0, 160, 0))
+            if fatalRight <= 5:
+                sleep(1)
+                finish = False
+                ball.rect.x = 250
+                ball.rect.y = 250
+                ball2.rect.x = 450
+                ball2.rect.y = 250
+            else:
+                window.blit(lose1, (ww/2.5, wh/2))
+                finish = True
+        if ball.rect.x >= ww-40 or ball2.rect.x >= ww-40:
+            fatalRight += 1
+            scoreL = font.render("Score: " + str(fatalRight), True, (0, 160, 0))
+            if fatalRight <= 5:
+                sleep(1)
+                finish = False
+                ball.rect.x = 250
+                ball.rect.y = 250
+                ball2.rect.x = 450
+                ball2.rect.y = 250
+            else:
+                window.blit(lose2, (ww/2.5, wh/2))
+                finish = True
         display.update()
         clock.tick(FPS)
+        
+    
